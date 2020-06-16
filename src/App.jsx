894ecbe721a92656089d8e615b603658e51be2a7
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-16 11:50:57 +0200

    adding sampleText.js, bootstrap and marked

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
 import './App.css';
 
-function App() {
-  return <div className="App" />;
+import marked from 'marked';
+
+import sampleText from './sampleText';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              className="form-control"
+              rows="35"
+              value={sampleText}
+            ></textarea>
+          </div>
+          <div className="col-sm-6">
+            <div>{sampleText}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

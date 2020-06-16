418e63fdda82476ee448a96d549cf752e128af67
nicholas570 <nicolas.mercier570@gmail.com>
2020-06-16 15:42:54 +0200

    adding methods and lifecycle methods

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,38 @@ import marked from 'marked';
 import sampleText from './sampleText';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: sampleText,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text');
+    if (text) {
+      this.setState({ text });
+    } else {
+      this.setState({ text: sampleText });
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state;
+    localStorage.setItem('text', text);
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  renderText(text) {
+    return marked(text, { sanitize: true });
+  }
+
   render() {
+    const { text } = this.state;
     return (
       <div className="container">
         <div className="row">
@@ -14,11 +45,12 @@ class App extends React.Component {
             <textarea
               className="form-control"
               rows="35"
-              value={sampleText}
-            ></textarea>
+              value={text}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="col-sm-6">
-            <div>{sampleText}</div>
+            <div dangerouslySetInnerHTML={{ __html: this.renderText(text) }} />
           </div>
         </div>
       </div>
